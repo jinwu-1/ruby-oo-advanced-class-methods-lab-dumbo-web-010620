5c997f65c0dd9f758b4bf4ea9a96296f8c2757d9
Jin Wu <jinn.wuu@gmail.com>
2020-01-12 02:59:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,33 +15,37 @@ class Song
     @@all << song
     song
   end
-  
+
   def self.new_by_name(name)
-    song = self.new 
+    song = self.new
     song.name = name
     song
   end
-  
+
   def self.create_by_name(name)
     song = self.create
     song.name = name
     song
   end
-    
+
   def self.find_by_name(name)
     @@all.find do |x|
       x.name == name
     end
   end
-  
+
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
-  
+
   def self.alphabetical
     @@all.sort_by do |x|
       x.name
     end
   end
-  
+
+  def self.new_from_filename
+
+  end
+
 end
